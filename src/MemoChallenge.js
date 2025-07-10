4d493fa944172d50db7435cf35025ffbc3fff067
Namnika <janbandhunamnika@gmail.com>
2025-07-10 11:07:25 +0530

    add rect.memo challenge

---
+++ src/MemoChallenge.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export const Child1 = React.memo(({ text }) => {
+  console.log("Child1 rendered from Memo challenge");
+  return <div>Child1 component rendered with text: {text}</div>;
+});
+
+export const Child2 = ({ count }) => {
+  console.log("Child2 rendered from Memo challenge");
+  return <div>Child2 component rendered with Result: {count}</div>;
+};
+
+const MemoChallenge = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <Child1 text="this is  pre-defined text" />
+      <Child2 count={count} />
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  );
+};
+
+export default MemoChallenge;
